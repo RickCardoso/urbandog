906503e8cf2f23e9631c7902acf99cf44edd2abf
Ricardo Cardoso <rickclsilva@gmail.com>
2017-08-25 13:39:48 -0300

    header and footer
    responsive header and footer

--- res/js/functions.js
+++ res/js/functions.js
@@ -14,4 +14,25 @@ $(document).ready(function() {
     }
   });
 
+  /* search-box actions */
+  // on search-icon click opens
+    $('.search-icon').click(function(e) {
+      if ($('.search-box').css('opacity') == 0) {
+        $('.search-box').animate({
+          width: '180px',
+          opacity: '1'
+        }, 600);
+        $('.search-box').focus();
+      }
+    });
+  // on focusOut closes if empty
+    $('.search-box').focusout(function() {
+      if ($('.search-box').val() == '') {
+        $('.search-box').animate({
+          width: '0',
+          opacity: '0'
+        }, 600);
+      }
+    });
+
 });
